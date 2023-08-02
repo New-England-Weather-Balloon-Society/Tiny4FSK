936bc0f1bc5957c98dc0f275d2c415f0fa5ec872
maxsrobotics <66796793+maxsrobotics@users.noreply.github.com>
2023-08-02 12:16:32 -0400

    Update util.ino

--- util.ino
+++ util.ino
@@ -1,19 +1,5 @@
 // Various utility functions
 
-#include <util/crc16.h>
-
-// Fast CRC16 code, using Atmel's optimized libraries!
-unsigned int crc16(unsigned char *string, unsigned int len) {
-	unsigned int i;
-	unsigned int crc;
-	crc = 0xFFFF; // Standard CCITT seed for CRC16.
-	// Calculate the sum, ignore $ sign's
-	for (i = 0; i < len; i++) {
-		crc = _crc_xmodem_update(crc,(uint8_t)string[i]);
-	}
-	return crc;
-}
-
 void PrintHex(char *data, uint8_t length, char *tmp){
  // Print char data as hex
  byte first ;
