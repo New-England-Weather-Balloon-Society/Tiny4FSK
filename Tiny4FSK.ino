59bec0c1dfd919b1f1678825ae8a99a312f2abfb
Max Kendall <66796793+maxsrobotics@users.noreply.github.com>
2024-06-08 20:38:17 -0400

    Update Tiny4FSK.ino

--- Tiny4FSK.ino
+++ Tiny4FSK.ino
@@ -43,8 +43,8 @@
 #include "voltage.h"
 #include "i2c_scan.h"
 #include "si4063.h"
+#include "4fsk_mod.h"
 #include <SPI.h>
-#include "Adafruit_ZeroTimer.h"
 
 // **********************
 // || Native USB Setup ||
@@ -56,8 +56,6 @@
 // *************************
 // || Object Declarations ||
 // *************************
-// SX1278 radio module instance
-// SX1278 radio = new Module(NSS_PIN, DIO0_PIN, RESET_PIN, DIO1_PIN);
 
 // New UBLOX object
 SFE_UBLOX_GNSS gps;
@@ -97,20 +95,12 @@ uint16_t packet_count = 1; // Packet counter
 #define NSEL 10
 #define SDN 11
 
-int count = 0;
-
-float freq = 100.0;
-
-volatile bool packetSent = 0;
-
-// Adafruit_ZeroTimer zerotimer = Adafruit_ZeroTimer(3);
-
 void setup()
 {
   Serial.begin(9600);
-  while(!Serial);
+  //while(!Serial);
 
-  // Initialize SX1278 radio with default settings
+  // Initialize Si4063 radio with default settings
   Serial.println(("Initializing Radio..."));
 
   // Pinmode Declarations
@@ -118,6 +108,8 @@ void setup()
   pinMode(SUCCESS_LED, OUTPUT);
   pinMode(NSEL, OUTPUT);
   pinMode(SDN, OUTPUT);
+
+  SPI.begin();
 
   // Initialize I2C for GPS
   Wire.begin();
@@ -197,8 +189,7 @@ void setup()
   // **********************
   // || Initialize Radio ||
   // **********************
-  // radio.beginFSK();
-  // fsk4_setup(&radio, FSK_FREQ, FSK_SPACING, FSK_BAUD);
+
   configureSi4063();
 #ifdef DEV_MODE
   Serial.println(F("Radio Initialized!"));
@@ -208,21 +199,8 @@ void setup()
   delay(1000);
   digitalWrite(SUCCESS_LED, LOW);
 #endif
-  /*   BME280setI2Caddress(0x76);
-    BME280setup(); */
 
-  /*  uint16_t divider = 8;
-    uint16_t compare = (48000000 / 8) / freq;
-    tc_clock_prescaler prescaler = TC_CLOCK_PRESCALER_DIV8;
-
-    zerotimer.enable(false);
-    zerotimer.configure(prescaler,                   // prescaler
-                        TC_COUNTER_SIZE_16BIT,       // bit width of timer/counter
-                        TC_WAVE_GENERATION_MATCH_PWM // frequency or PWM mode
-    );
-
-    zerotimer.setCompare(0, compare);
-    zerotimer.setCallback(true, TC_CALLBACK_CC_CHANNEL0, packetHandler); */
+  BME280setup();
 }
 
 void loop()
@@ -241,6 +219,15 @@ void loop()
 #endif
   pkt_len = build_horus_binary_packet_v2(rawbuffer);
   coded_len = horus_l2_encode_tx_packet((unsigned char *)codedbuffer, (unsigned char *)rawbuffer, pkt_len);
+
+  Serial.print(F("Encoded Length (bytes): "));
+  Serial.println(coded_len);
+  Serial.print("Coded Buffer: ");
+  PrintHex(codedbuffer, coded_len, debugbuffer);
+  Serial.println(debugbuffer);
+  Serial.print("Raw Buffer: ");
+  PrintHex(rawbuffer, coded_len, debugbuffer);
+  Serial.println(debugbuffer);
   // *******************
   // || Transmit Time ||
   // *******************
@@ -248,20 +235,8 @@ void loop()
   Serial.println(F("Transmitting Horus Binary v2 Packet"));
 #endif
 
-  //SPI.beginTransaction(SPISettings(10000000, MSBFIRST, SPI_MODE0));
-  //si4063_enable_tx();
-  //SPI.endTransaction();
-
   fsk4_preamble(8);
   fsk4_write(codedbuffer, coded_len);
-
-  //SPI.beginTransaction(SPISettings(10000000, MSBFIRST, SPI_MODE0));
-  //si4063_wait_for_cts();
-  
-  //si4063_set_state(SI4063_STATE_READY);
-
-  //si4063_wait_for_cts();
-  //SPI.endTransaction();
 
 #ifdef DEV_MODE
   Serial.println(F("Transmission complete!"));
@@ -290,7 +265,7 @@ int build_horus_binary_packet_v2(char *buffer)
   struct HorusBinaryPacketV2 BinaryPacketV2;
 
   // Wake up the BME280
-  // BME280setup();
+  BME280setup();
 
   // Fill with GPS readings.
   BinaryPacketV2.Hours = gps.getHour();
@@ -298,7 +273,7 @@ int build_horus_binary_packet_v2(char *buffer)
   BinaryPacketV2.Seconds = gps.getSecond();
   BinaryPacketV2.Latitude = gps.getLatitude() / 10000000.00;
   BinaryPacketV2.Longitude = gps.getLongitude() / 10000000.00;
-  BinaryPacketV2.Altitude = gps.getAltitudeMSL() / 100;
+  BinaryPacketV2.Altitude = gps.getAltitudeMSL() / 1000.00;
   BinaryPacketV2.Speed = gps.getGroundSpeed();
   BinaryPacketV2.Sats = gps.getSIV();
 #ifdef STATUS_LED
@@ -309,16 +284,16 @@ int build_horus_binary_packet_v2(char *buffer)
 
   // Non-GPS values
   BinaryPacketV2.PayloadID = HORUS_ID;
-  BinaryPacketV2.Counter = packet_count;
+  BinaryPacketV2.Counter = 0;
   BinaryPacketV2.BattVoltage = readVoltage();
-  BinaryPacketV2.Temp = 20; // BME280temperature() / 100.00;
+  BinaryPacketV2.Temp = BME280temperature() / 100.00;
 
   // User-Customizable Fields
-  BinaryPacketV2.dummy1 = 1; // BME280pressure() / 100.00;
-  BinaryPacketV2.dummy2 = 2; // BME280humidity() / 100.00;
-  BinaryPacketV2.dummy3 = 3;
-  BinaryPacketV2.dummy4 = 4;
-  BinaryPacketV2.dummy5 = 5;
+  BinaryPacketV2.dummy1 = BME280pressure() / 100.00;
+  BinaryPacketV2.dummy2 = BME280humidity() / 100.00;
+  BinaryPacketV2.dummy3 = 0;
+  BinaryPacketV2.dummy4 = 0;
+  BinaryPacketV2.dummy5 = 0;
 
   // End the packet off with a CRC checksum.
   BinaryPacketV2.Checksum = (uint16_t)crc16((unsigned char *)&BinaryPacketV2, sizeof(BinaryPacketV2) - 2);
@@ -329,20 +304,22 @@ int build_horus_binary_packet_v2(char *buffer)
   Serial.print(BinaryPacketV2.Latitude, 7);
   Serial.print(", Longitude: ");
   Serial.print(BinaryPacketV2.Longitude, 7);
+  Serial.print(", Altitude MSL: ");
+  Serial.print(gps.getAltitudeMSL() / 1000.00);
   Serial.print(", Sats: ");
   Serial.print(gps.getSIV());
   Serial.print(", Voltage: ");
-  Serial.println(readVoltage());
-/*   Serial.print(", Temperature: ");
+  Serial.print(readVoltage());
+  Serial.print(", Temperature: ");
   Serial.print(BME280temperature() / 100.00);
   Serial.print(", Pressure: ");
   Serial.print(BME280pressure() / 100.00);
   Serial.print(", Humidity: ");
-  Serial.println(BME280humidity() / 100.00); */
+  Serial.println(BME280humidity() / 100.00);
 #endif
 
   // Put the BME280 back to sleep
-  // BME280sleep();
+  BME280sleep();
 
   // Copy the binary packet to the buffer
   memcpy(buffer, &BinaryPacketV2, sizeof(BinaryPacketV2));
@@ -362,12 +339,12 @@ void configureSi4063()
 
   radio_parameters rf_params;
   rf_params.frequency_hz = 434000000;
-  rf_params.power = 0x01;
+  rf_params.power = 0x30;
   rf_params.type = SI4063_MODULATION_TYPE_CW;
   rf_params.offset = 0;
   rf_params.deviation_hz = 0x00;
 
-  SPI.beginTransaction(SPISettings(10000000, MSBFIRST, SPI_MODE0));
+  //SPI.beginTransaction(SPISettings(10000000, MSBFIRST, SPI_MODE0));
   if (si4063_init(rf_params, si_params) != HAL_OK) {
     // Handle initialization error
     Serial.println("Initialization Error!");
@@ -377,76 +354,24 @@ void configureSi4063()
   delay(100);
   Serial.print("Part Number: 0x"); Serial.println(si4063_read_part_info(), HEX);
   si4063_enable_tx();
-  SPI.endTransaction();
-}
-
-void PrintHex(char *data, uint8_t length, char *tmp)
-{
-  // Print char data as hex
-  byte first;
-  int j = 0;
-  for (uint8_t i = 0; i < length; i++)
-  {
-    first = ((uint8_t)data[i] >> 4) | 48;
-    if (first > 57)
-      tmp[j] = first + (byte)39;
-    else
-      tmp[j] = first;
-    j++;
-
-    first = ((uint8_t)data[i] & 0x0F) | 48;
-    if (first > 57)
-      tmp[j] = first + (byte)39;
-    else
-      tmp[j] = first;
-    j++;
-  }
-  tmp[length * 2] = 0;
-}
-
-size_t fsk4_writebyte(uint8_t b)
-{
-  // Send symbols MSB first.
-  for (int k = 0; k < 4; k++)
-  {
-    // Extract 4FSK symbol (2 bits)
-    uint8_t symbol = (b & 0xC0) >> 6;
-    uint32_t start = micros();
-    // Modulate
-    SPI.beginTransaction(SPISettings(10000000, MSBFIRST, SPI_MODE0));
-    si4063_set_frequency_offset(22 * symbol);
-    SPI.endTransaction();
-
-    while(micros() - start < 10000) {
-      yield();
-    }
-    // Shift to next symbol.
-    b = b << 2;
-  }
-
-  return (1);
-}
-
-void fsk4_write(char *buff, size_t len)
-{
-  for (size_t i = 0; i < len; i++)
-  {
-    fsk4_writebyte(buff[i]);
-  }
-Serial.println("433");
-  //SPI.beginTransaction(SPISettings(10000000, MSBFIRST, SPI_MODE0));
-  //si4063_inhibit_tx();
   //SPI.endTransaction();
 }
-void fsk4_preamble(uint8_t len)
-{
-  int k;
-  for (k = 0; k < len; k++)
-  {
-    fsk4_writebyte(0x1B);
-  }
-}
 
-void fsk4_idle() {
-  si4063_set_frequency_offset(0);
+void PrintHex(char *data, uint8_t length, char *tmp){
+ // Print char data as hex
+ byte first ;
+ int j=0;
+ for (uint8_t i=0; i<length; i++) 
+ {
+   first = ((uint8_t)data[i] >> 4) | 48;
+   if (first > 57) tmp[j] = first + (byte)39;
+   else tmp[j] = first ;
+   j++;
+
+   first = ((uint8_t)data[i] & 0x0F) | 48;
+   if (first > 57) tmp[j] = first + (byte)39; 
+   else tmp[j] = first;
+   j++;
+ }
+ tmp[length*2] = 0;
 }
